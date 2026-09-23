81464909ff91dbfae451f8b3bd4caf180c8d1c9f
Алеся <alesssftf@gmail.com>
2026-09-23 19:56:51 +0300

    поправила текст и всунула ссылки на презы

--- js/config.js
+++ js/config.js
@@ -9,21 +9,20 @@ const CONFIG = {
   email:    '',          // например 'alesya@mail.ru'
 
   // Технологии — впишите свой стек
-  stack: ['HTML', 'CSS', 'JavaScript', 'Python'],
+  stack: ['HTML', 'CSS', 'JavaScript', 'Python','Node.js', 'React', "Django", 'SQL', 'PostgreSQL', 'MongoDB', 'Git', 'Docker'],
 
-  // Проекты. Чтобы добавить новый — скопируйте блок { ... } и поменяйте текст.
   projects: [
     {
       title: 'CRM для агрегатора заявок',
       role:  'Команда разработки',
       desc:  'Система, которая собирает заявки из разных источников, распределяет их между исполнителями и показывает статус каждой.',
-      url:   ''   // ссылка на репозиторий
+      url:   'https://docs.google.com/presentation/d/16yZVwII9pjvKSot1GM-i-0wvH7UrpT1DziM3VpMRDdU/edit?usp=sharing '   // ссылка на репозиторий
     },
     {
       title: 'Видеоконференции во внутренней сети',
       role:  'Команда разработки',
       desc:  'Сервис видеосвязи, который работает внутри корпоративной сети компании — без внешних платформ и с данными, которые не покидают компанию.',
-      url:   ''
+      url:   'https://docs.google.com/presentation/d/18D_ciZPjcyScoSYJiglOrLQ3TXoHy8blSz_BJmtouOo/edit?usp=sharing '
     },
     // {
     //   title: 'Название проекта',
